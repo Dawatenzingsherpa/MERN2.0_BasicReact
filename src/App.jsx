b828c37b092c00c1e33580db6d01b45adf9a9fbb
Dawatenzingsherpa <dawatenzingsherpa907@gmail.com>
2026-06-02 16:38:56 +0545

    Navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Navbar from './components/Navbar'
 function App() {
 
   return (
     <>
-  
+      <Navbar />
+
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav className="navbar">
+      <div className="logo">
+        <h2>📚 BookVerse</h2>
+      </div>
+
+      <ul className="nav-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/books">Books</a></li>
+        <li><a href="/categories">Categories</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+
+      <div className="nav-actions">
+        <input
+          type="text"
+          placeholder="Search books..."
+          className="search-box"
+        />
+        <button className="login-btn">Login</button>
+      </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
